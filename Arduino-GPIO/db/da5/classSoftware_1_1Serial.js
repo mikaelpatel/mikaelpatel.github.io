b5b9d3a30bd902aad8fb4f073253ce89321fdc3b
mikaelpatel <mikael.patel@gmail.com>
2017-09-05 23:25:50 +0200

    Version 1.3

--- Arduino-GPIO/db/da5/classSoftware_1_1Serial.js
+++ Arduino-GPIO/db/da5/classSoftware_1_1Serial.js
@@ -3,6 +3,7 @@ var classSoftware_1_1Serial =
     [ "Serial", "db/da5/classSoftware_1_1Serial.html#a694ade4d4f6390f2b2e509a23961b03a", null ],
     [ "available", "db/da5/classSoftware_1_1Serial.html#ae7d9a5bb414f4747f749f057ea8ff8ad", null ],
     [ "begin", "db/da5/classSoftware_1_1Serial.html#aacf522e85b27f8edbccfdb1d187fdea5", null ],
+    [ "operator bool", "db/da5/classSoftware_1_1Serial.html#a714b0e74ad67f57a389e81ff2d688ca1", null ],
     [ "peek", "db/da5/classSoftware_1_1Serial.html#af6e5c918bd38edfd93f211ba8600d63c", null ],
     [ "read", "db/da5/classSoftware_1_1Serial.html#ac25e583d87b35c880700ac81b414e0c6", null ],
     [ "write", "db/da5/classSoftware_1_1Serial.html#a27cccf6037cb9b3e4b250d6d1a836744", null ],
--- Arduino-GPIO/search/all_8.js
+++ Arduino-GPIO/search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['operator_20bool',['operator bool',['../d7/dde/classGPIO.html#aacaa2b56ea6dfc8b58fc3a1b636330af',1,'GPIO']]],
+  ['operator_20bool',['operator bool',['../d7/dde/classGPIO.html#aacaa2b56ea6dfc8b58fc3a1b636330af',1,'GPIO::operator bool()'],['../db/da5/classSoftware_1_1Serial.html#a714b0e74ad67f57a389e81ff2d688ca1',1,'Software::Serial::operator bool()']]],
   ['operator_3c_3c',['operator&lt;&lt;',['../de/d8f/classSRPO.html#a4b6b20becc3957d649aff7e822a2619b',1,'SRPO']]],
   ['operator_3d',['operator=',['../d7/dde/classGPIO.html#a2abddc963c8d4ea00dae803cc98578f9',1,'GPIO']]],
   ['operator_3e_3e',['operator&gt;&gt;',['../d7/da4/classSRPI.html#a2cfa824a8be70706c77a13baffa08435',1,'SRPI']]],
