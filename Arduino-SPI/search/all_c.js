a3581a9d8feb226638555501bcf7a0904792f58f
mikaelpatel <mikael.patel@gmail.com>
2017-09-18 01:48:48 +0200

    Performance improvement

--- Arduino-SPI/search/all_c.js
+++ Arduino-SPI/search/all_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['write',['WRITE',['../d2/d57/classSRAM.html#a6b79da6bf17870a2e7a468920f7e7396a0e7dd0393ae5b7e085bc252c0826e919',1,'SRAM::WRITE()'],['../d2/d57/classSRAM.html#a5eba38d6e99f3bd8bbc272fc4168ad3e',1,'SRAM::write(uint32_t addr, const void *buf, size_t count)'],['../d9/db9/classHardware_1_1SPI.html#ab7aa0ca5510923d53e8925106342711a',1,'Hardware::SPI::write()'],['../da/d8d/classSPI.html#a3848c8f7c74e59e9f14ebf50a4ce8dcf',1,'SPI::write()'],['../d5/d8d/classSPI_1_1Device.html#a8d560b1e7a21afc67865376a568b67da',1,'SPI::Device::write()']]],
+  ['write',['WRITE',['../d2/d57/classSRAM.html#a6b79da6bf17870a2e7a468920f7e7396a0e7dd0393ae5b7e085bc252c0826e919',1,'SRAM::WRITE()'],['../d2/d57/classSRAM.html#adb6ba9a70a2b894c431e5228f5d35d58',1,'SRAM::write(uint32_t dest, const void *src, size_t count)'],['../d9/db9/classHardware_1_1SPI.html#ab7aa0ca5510923d53e8925106342711a',1,'Hardware::SPI::write()'],['../da/d8d/classSPI.html#a3848c8f7c74e59e9f14ebf50a4ce8dcf',1,'SPI::write()'],['../d5/d8d/classSPI_1_1Device.html#a8d560b1e7a21afc67865376a568b67da',1,'SPI::Device::write()']]],
   ['wrmr',['WRMR',['../d2/d57/classSRAM.html#a6b79da6bf17870a2e7a468920f7e7396a4e9cab2fc099a1b3758cb7aeff77d29c',1,'SRAM']]]
 ];
--- Arduino-SPI/search/functions_4.js
+++ Arduino-SPI/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['read',['read',['../d2/d57/classSRAM.html#a4b187fb79885ecd8e890a7eeceb48484',1,'SRAM::read()'],['../d9/db9/classHardware_1_1SPI.html#a0fd70c07aadea66eb0e2a6e0ba4bfa5e',1,'Hardware::SPI::read()'],['../da/d8d/classSPI.html#a800703e9e3bc12de9577e32e44988a78',1,'SPI::read()'],['../d5/d8d/classSPI_1_1Device.html#ac228d803780060c8e0ba325cc8be5170',1,'SPI::Device::read()']]],
+  ['read',['read',['../d2/d57/classSRAM.html#ac3bfcd75c16f21b1433d6b595f31d77d',1,'SRAM::read()'],['../d9/db9/classHardware_1_1SPI.html#a0fd70c07aadea66eb0e2a6e0ba4bfa5e',1,'Hardware::SPI::read()'],['../da/d8d/classSPI.html#a800703e9e3bc12de9577e32e44988a78',1,'SPI::read()'],['../d5/d8d/classSPI_1_1Device.html#ac228d803780060c8e0ba325cc8be5170',1,'SPI::Device::read()']]],
   ['release',['release',['../d9/db9/classHardware_1_1SPI.html#a19e8c242356ae6768b649fd68902f033',1,'Hardware::SPI::release()'],['../d1/de2/classSoftware_1_1SPI.html#a7b8e93719d90db69f8fdbef8a55fbd3f',1,'Software::SPI::release()'],['../da/d8d/classSPI.html#aa8753d4730ab2daa3a1f093ef5f88f82',1,'SPI::release()'],['../d5/d8d/classSPI_1_1Device.html#a131393b556e6591565dc7e8cfad4213a',1,'SPI::Device::release()']]]
 ];
